6d8ea7b6b7eb4d0cc62b07e558d7f6db2a68cf72
jonathanberry <jonathanberry45@gmail.com>
2023-08-02 15:04:25 +0100

    added date builder and array of year and months

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  ;
+  };
   return (
     <div className="app">
       <main>
@@ -18,7 +24,7 @@ function App() {
         </div>
         <div className='location-box'>
           <div className="location">New York City. US</div>
-          <div className='date'></div>
+          <div className='date'>{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
