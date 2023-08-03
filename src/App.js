53e9329d0fd15cb5848dd345a4b512a264c1933f
jonathanberry <jonathanberry45@gmail.com>
2023-08-03 22:32:31 +0100

    search function added to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import React, {useState} from 'react';
 const api = {
   key: "922b24701c3a2a910e1c31e283790816",
   base:"https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter"){
+      fetch{`${api.base}weather?q=${query}&units=metric&APPID={api.key}`}
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
